ce6dfa0231ec32b2169f2b7464b8dc989c2803a5
killian moudery <killian.moudery@reelevant.com>
2026-03-09 18:23:52 +0100

    feat: add id generator injection

--- service/option.go
+++ service/option.go
@@ -7,28 +7,34 @@ import (
 
 type Option func(*Application)
 
+func WithGenerator(generator Generator) Option {
+	return func(a *Application) {
+		a.generator = generator
+	}
+}
+
 func WithIdentifier(name string, version string) Option {
-  return func(a *Application) {
-    a.name = name
-    a.version = version
-    a.id = fmt.Sprintf("%s/%s", name, version)
-  }
+	return func(a *Application) {
+		a.name = name
+		a.version = version
+		a.id = fmt.Sprintf("%s/%s", name, version)
+	}
 }
 
 func WithConfig(config Config) Option {
-  return func(a *Application) {
-    a.conf = config
-  }
+	return func(a *Application) {
+		a.conf = config
+	}
 }
 
 func WithService(services ...Service) Option {
-  return func(a *Application) {
-    a.services = services
-  }
+	return func(a *Application) {
+		a.services = services
+	}
 }
 
 func WithLogger(logger *slog.Logger) Option {
-  return func(a *Application) {
-    a.logger = logger
-  }
+	return func(a *Application) {
+		a.logger = logger
+	}
 }
